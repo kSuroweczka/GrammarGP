9156635173a8672b64e0a1a190d28cd754e98d94
kSuroweczka <92479853+kSuroweczka@users.noreply.github.com>
2024-01-04 15:39:09 +0100

    runGP

--- runGP.py
+++ runGP.py
@@ -7,7 +7,21 @@ gp = GP(task_name=task_name, params=params)
 
 gp.run(fitness_function=fitness_27_Bench)
 
+for i in range(1,11):
+    for j in range(1,5):
+        task_name = f'Bool_{i}_{j}'
+#         exec(f'x= fitness_Bool')
+#         res = x()
+        params = Params(max_depth=2, popsize=50, min_rand=-5, max_rand=5, generations=100)
+        gp = GP(task_name=task_name, params=params)
+        gp.run(fitness_function=fitness_Bool)
 
+for i in [1,17,27]:
+    task_name = f'{i}_Bench'
+    exec(f'x= fitness_{i}_Bench')
+    params = Params(max_depth=2, popsize=50, min_rand=-5, max_rand=5, generations=100)
+    gp = GP(task_name=task_name, params=params)
+    gp.run(fitness_function=exec(f'fitness_{i}_Bench'))
 
 
 
