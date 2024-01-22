6273cbc58acc0fa25f32603fec8e5a830f9d40e7
kSuroweczka <92479853+kSuroweczka@users.noreply.github.com>
2024-01-22 10:46:11 +0100

    some new fitness functions for Bool problems

--- library/Tasks/fitness_functions.py
+++ library/Tasks/fitness_functions.py
@@ -3,6 +3,10 @@ import numpy as np
 import math
 
 
+### solved problems:
+### 1_1_A, 1_1_D, 1_1_F, 1_2_A,
+
+
 def fitness_1_1_A(individual: Program):
     output = np.array(individual.output_data)
     if output.shape[0] == 0:
@@ -22,7 +26,7 @@ def fitness_1_1_B(individual: Program):
     elif 789.0 in output:
         fitness = 0.0
     else:
-        fitness = -np.min(np.abs(output - 789.0))
+        fitness = -np.average(np.abs(output - 789.0)) - 300.0
     return fitness
 
 
@@ -37,12 +41,25 @@ def fitness_1_1_C(individual: Program):
         fitness = -np.min(np.abs(output - 31415.0))
     return fitness
 
+###--------------------------------------------
+###                    1_1_D
+###--------------------------------------------
+
+# def fitness_1_1_D(individual: Program):
+#     output = np.array(individual.output_data)
+#     if output.shape[0] == 0:
+#         fitness = -1000
+#     elif 1.0 in output:
+#         fitness = 0.0
+#     else:
+#         fitness = -abs(output[0] - 1.0)
+#     return fitness
 
 def fitness_1_1_D(individual: Program):
     output = np.array(individual.output_data)
     if output.shape[0] == 0:
         fitness = -1000
-    elif 1.0 in output:
+    elif 1.0 in output and output[0] == 1.0:
         fitness = 0.0
     else:
         fitness = -abs(output[0] - 1.0)
@@ -59,7 +76,12 @@ def fitness_1_1_E(individual: Program):
         fitness = -abs(output[0] - 789.0)
     return fitness
 
+###--------------------------------------------
+###                    1_1_F
+###--------------------------------------------
 
+
+###------ problem solved w 7 generacji
 def fitness_1_1_F(individual: Program):
     output = np.array(individual.output_data)
     if output.shape[0] == 0:
@@ -72,6 +94,34 @@ def fitness_1_1_F(individual: Program):
     fitness += (output.shape[0] - 1) * -10
     return fitness
 
+###--------------------------------------------
+###                    1_2_A
+###--------------------------------------------
+
+def fitness_1_2_A(individual: Program):
+    output = individual.output_data
+    input_data = individual.input_data
+    expected = input_data[0] + input_data[1]
+
+    fitness = -100
+    input = individual.input
+#     if len(input) < 2:
+#         print("input too short")
+#         fitness = -100
+    if len(output) == 0:
+        fitness = -100.0
+    elif len(output) == 1:
+        if output[0] == expected:
+            fitness = 0.0
+        else:
+            fitness = (-1) * np.abs(expected - output[0])
+    else:
+        avg = np.average(output)
+        fitness = (-1) * np.abs(avg - expected)
+    fitness += abs(len(output) - 1) * -20
+    fitness += abs(len(input) - 2) * -30
+
+    return fitness
 
 def fitness_1_2_A_C(individual: Program):
     output = individual.output_data
@@ -92,7 +142,7 @@ def fitness_1_2_A_C(individual: Program):
     else:
         avg = np.average(output)
         fitness = (-1) * np.abs(avg - expected)
-    fitness += abs(len(output) - 1) * -20
+    fitness += abs(len(output) - 1) * -30
     fitness += abs(len(input) - 2) * -20
 
     return fitness
@@ -233,10 +283,96 @@ def fitness_4_11_7(individual: Program):
     return fitness
 
 
-def fitness_Bool(individual: Program):
+# def fitness_Bool(individual: Program):
+#     output = individual.output_data
+#     input_data = np.array(individual.input)
+#     expected = individual.task.test_cases[0].output_data
+#     k = len(expected)
+#
+#     if len(output) == 0:
+#         fitness = -100.0
+#     elif len(output) == k:
+#         if output == expected:
+#             fitness = 0.0
+#         else:
+#             fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+#     else:
+#         fitness = -50.0
+#     fitness += abs(len(output) - k) * -10
+#
+#     return fitness
+
+# def fitness_Bool_1(individual: Program):
+#     output = individual.output_data
+#     input_data = np.array(individual.input)
+#     expected = individual.task.test_cases[0].output_data
+#     k = len(expected)
+#
+#     if len(output) == 0:
+#         fitness = -100.0
+#     elif len(output) == k:
+#         if output == expected:
+#             fitness = 0.0
+#         else:
+#             fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+#     else:
+#         fitness = -50.0
+#     fitness += abs(len(output) - k) * -10
+#     fitness += abs(len(input_data) - 1) * -10
+#
+#     return fitness
+
+# def fitness_Bool_2_1(individual: Program):
+#     output = individual.output_data
+#     input_data = np.array(individual.input)
+#     expected = individual.task.test_cases[0].output_data
+#     k = len(expected)
+#
+#     if len(output) == 0:
+#         fitness = -100.0
+#     elif len(output) == k:
+#         if output == expected:
+#             fitness = 0.0
+#         else:
+#             fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+#     else:
+#         fitness = -50.0
+#     fitness += abs(len(output) - k) * -10
+#     fitness += abs(len(input_data) - 2) * -10
+#
+#     return fitness
+
+# def fitness_Bool_2_24(individual: Program):
+#     output = individual.output_data
+#     input_data = np.array(individual.input)
+#     expected = individual.task.test_cases[0].output_data
+#     k = len(expected)
+#
+#     if len(output) == 0:
+#         fitness = -100.0
+#     elif len(output) == k:
+#         if output == expected:
+#             fitness = 0.0
+#         else:
+#             fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+#     else:
+#         fitness = -50.0
+#     fitness += abs(len(output) - 1) * -10
+#     fitness += abs(len(input_data) - 2) * -10
+#
+#     return fitness
+
+
+def fitness_Bool_NOT(individual: Program):
     output = individual.output_data
     input_data = np.array(individual.input)
-    expected = individual.task.test_cases[0].output_data
+
+    n = len(individual.task.test_cases[0].input_data)  ## expected input length
+
+    expected =[]
+    for i in range(len(input_data)):
+        expected.append(int(not input_data[i]))
+
     k = len(expected)
 
     if len(output) == 0:
@@ -249,8 +385,84 @@ def fitness_Bool(individual: Program):
     else:
         fitness = -50.0
     fitness += abs(len(output) - k) * -10
+    fitness += abs(len(input_data) - n) * -10
 
     return fitness
+
+def fitness_Bool_AND(individual: Program):
+    output = individual.output_data
+    input_data = np.array(individual.input)
+
+    n = len(individual.task.test_cases[0].input_data)  ## expected input length
+
+    expected =[]
+    expected.append(int(all(input_data)))
+
+    k = len(expected)
+
+    if len(output) == 0:
+        fitness = -100.0
+    elif len(output) == k:
+        if output == expected:
+            fitness = 0.0
+        else:
+            fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+    else:
+        fitness = -50.0
+    fitness += abs(len(output) - k) * -10
+    fitness += abs(len(input_data) - n) * -10
+
+    return fitness
+
+def fitness_Bool_OR(individual: Program):
+    output = individual.output_data
+    input_data = np.array(individual.input)
+
+    n = len(individual.task.test_cases[0].input_data)  ## expected input length
+
+    expected =[]
+    expected.append(int(any(input_data)))
+
+    k = len(expected)
+
+    if len(output) == 0:
+        fitness = -100.0
+    elif len(output) == k:
+        if output == expected:
+            fitness = 0.0
+        else:
+            fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+    else:
+        fitness = -50.0
+    fitness += abs(len(output) - k) * -10
+    fitness += abs(len(input_data) - n) * -10
+
+    return fitness
+
+def fitness_Bool_XOR(individual: Program):
+    output = individual.output_data
+    input_data = np.array(individual.input)
+
+    n = len(individual.task.test_cases[0].input_data)  ## expected input length
+
+    expected =[]
+    expected.append(int(sum(input_data) % 2))
+    k = len(expected)
+
+    if len(output) == 0:
+        fitness = -100.0
+    elif len(output) == k:
+        if output == expected:
+            fitness = 0.0
+        else:
+            fitness = (-1) * np.abs(np.subtract(output,expected)).sum()
+    else:
+        fitness = -50.0
+    fitness += abs(len(output) - k) * -10
+    fitness += abs(len(input_data) - n) * -10
+
+    return fitness
+
 
 def fitness_1_Bench(individual: Program):
     output = individual.output_data
--- runGP.py
+++ runGP.py
@@ -1,11 +1,11 @@
 from library.Solver.GP import *
 from library.Tasks.fitness_functions import *
 
-task_name = '1_1_F'
+task_name = 'Bool_3_4'
 params = Params(max_depth=2, popsize=50, min_rand=-5, max_rand=5, generations=100)
 gp = GP(task_name=task_name, params=params)
 
-gp.run(fitness_function=fitness_1_1_F)
+gp.run(fitness_function=fitness_Bool_XOR)
 
 # for i in range(1,11):
 #     for j in range(1,5):
